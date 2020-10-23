db5856decf5da6fe0df2ebe87f3a65c39b644d0e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-10-23 13:06:02 +0300

    Added teacher solution for task: MeetupPageComponent

---
+++ 02-components/02-MeetupPageComponent/MeetupInfo.js
@@ -0,0 +1,47 @@
+export const MeetupInfo = {
+  name: 'MeetupInfo',
+
+  template: `<ul class="info-list">
+      <li>
+        <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
+        {{ organizer }}
+      </li>
+      <li>
+        <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
+        {{ place }}
+      </li>
+      <li>
+        <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
+        <time :datetime="ISODate">{{ localDate }}</time>
+      </li>
+    </ul>`,
+
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+
+  computed: {
+    localDate() {
+      return new Date(this.date).toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+
+    ISODate() {
+      return new Date(this.date).toISOString().substr(0, 10);
+    },
+  },
+};
