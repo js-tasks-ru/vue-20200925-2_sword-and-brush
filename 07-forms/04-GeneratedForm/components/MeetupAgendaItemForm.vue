495c24cd227397ba035444882ea57d4420c34d1f
Red-Warrior <sword_and_brush@mail.ru>
2020-12-16 19:37:05 +0300

    GeneratedForm

--- 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
@@ -1,46 +1,56 @@
 <template>
   <div class="form-section form-section_inner">
-    <button type="button" class="remove-button">
+    <button @click="$emit('remove')" type="button" class="remove-button">
       <img src="../assets/icons/icon-trash.svg" alt="trash" />
     </button>
 
     <div class="form-group">
-      <dropdown-button title="Тип" :options="$options.agendaItemTypes" />
+      <dropdown-button
+        title="Тип"
+        :options="$options.agendaItemTypes"
+        v-model="localAgenda.type"
+      />
     </div>
 
     <div class="form__row">
       <div class="form__col">
         <div class="form-group">
           <label class="form-label">Начало</label>
-          <app-input type="time" placeholder="00:00" />
+          <app-input
+            type="time"
+            placeholder="00:00"
+            v-model="localAgenda.startsAt"
+            @click="oldStartAt = $event.target.value"
+            @change="changeAgendaStartsAt($event)"
+          />
         </div>
       </div>
       <div class="form__col">
         <div class="form-group">
           <label class="form-label">Окончание</label>
-          <app-input type="time" placeholder="00:00" />
+          <app-input
+            type="time"
+            placeholder="00:00"
+            v-model="localAgenda.endsAt"
+          />
         </div>
       </div>
     </div>
 
-    <div class="form-group">
-      <label class="form-label">Заголовок</label>
-      <app-input />
+    <div v-for="agendaField in selectedAgenda[localAgenda.type]"
+         :key="agendaField.field"
+         class="form-group">
+      <label class="form-label">{{ agendaField.title }}</label>
+      <component
+        :options="agendaField.props.options"
+        :multiline="agendaField.props.multiline"
+        :is="agendaField.component"
+        v-model="localAgenda[agendaField.field]"
+        @change="changeAgendaItem"
+      >
+      </component>
     </div>
-    <div class="form-group">
-      <label class="form-label">Описание</label>
-      <app-input multiline />
-    </div>
-    <div class="form-group">
-      <label class="form-label">Язык</label>
-      <dropdown-button
-        :options="[
-          { value: null, text: 'Не указано' },
-          { value: 'RU', text: 'RU' },
-          { value: 'EN', text: 'EN' },
-        ]"
-      />
-    </div>
+
   </div>
 </template>
 
@@ -59,6 +69,47 @@ export default {
 
   agendaItemTypes: getAgendaItemTypes(),
   fieldSpecifications: getAgendaItemsFieldSpecifications(),
+
+  data() {
+    return {
+      localAgenda: this.agendaItem,
+      selectedAgenda: getAgendaItemsFieldSpecifications(),
+      oldStartAt: this.agendaItem.startsAt,
+    };
+  },
+
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  methods: {
+    changeAgendaItem() {
+      this.$emit('update:agendaItem', this.localAgenda);
+    },
+
+    changeAgendaStartsAt(newValue) {
+      let oldStartAtMinutes = this.oldStartAt.split(':').reduce((acc, value) => +acc * 60 + +value);
+      let newStartAtMinutes = newValue.split(':').reduce((acc, value) => +acc * 60 + +value);
+      let oldEndsMinutes = this.localAgenda.endsAt.split(':').reduce((acc, value) => +acc * 60 + +value);
+      let newEndsAtMinutes = (oldEndsMinutes + (newStartAtMinutes - oldStartAtMinutes) + 24 * 60) % (24 * 60);
+
+      let hours = (newEndsAtMinutes / 60).toFixed(0);
+      let minutes = newEndsAtMinutes % 60;
+
+      if (hours < 10) {
+        hours = '0' + hours;
+      }
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      }
+
+      this.localAgenda.endsAt = `${hours}:${minutes}`;
+      this.changeAgendaItem();
+    },
+  },
 };
 </script>
 
