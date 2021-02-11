da6677b8523e2be37f8215e06642a594aab70a8f
Red-Warrior <sword_and_brush@mail.ru>
2021-02-11 22:44:33 +0300

    DateInput

--- 06-deep-vue/03-DateInput/components/DateInput.vue
+++ 06-deep-vue/03-DateInput/components/DateInput.vue
@@ -1,5 +1,11 @@
 <template>
-  <AppInput>
+  <AppInput
+    @input.native="updatedDate($event.target)"
+    :value="formattedDate"
+    :type="type"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
     <!-- Так можно передать все слоты в дочерний компонент -->
     <template v-for="slot of Object.keys($slots)" v-slot:[slot]>
       <slot :name="slot" />
@@ -18,6 +24,95 @@ export default {
   props: {
     // Чтобы value не было в списке $attrs, его можно указать в списке параметров
     value: {},
+    type: {
+      type: String,
+      default: 'date',
+      validator: (value) => ['date', 'time', 'datetime-local'].includes(value),
+    },
+    valueAsNumber: {
+      type: Number,
+    },
+    valueAsDate: {
+      type: Date,
+    },
+  },
+  methods: {
+    updatedDate(value) {
+      let inputValueAsNumber = value.valueAsNumber;
+      if (inputValueAsNumber.length <= 8) {
+        let timeValue = inputValueAsNumber.split(':');
+        if (timeValue.length === 2) {
+          inputValueAsNumber = new Date(this.actualParameter).setUTCHours(
+            timeValue[0],
+            timeValue[1],
+          );
+        } else {
+          inputValueAsNumber = new Date(this.actualParameter).setUTCHours(
+            timeValue[0],
+            timeValue[1],
+            timeValue[2],
+          );
+        }
+      }
+      let inputValueAsDate = inputValueAsNumber.valueAsDate
+        ? inputValueAsNumber.valueAsDate
+        : +inputValueAsNumber;
+      this.$emit('update:valueAsNumber', +new Date(inputValueAsNumber));
+      this.$emit('update:valueAsDate', new Date(inputValueAsDate));
+    },
+
+    getDate(value) {
+      let monthAndDay = [
+        new Date(value).getUTCMonth() + 1,
+        new Date(value).getUTCDate(),
+      ].map((item) => item.toString().padStart(2, '0'));
+      if (monthAndDay[0] === '00') {
+        monthAndDay[0] = '12';
+      }
+      return `${new Date(value).getUTCFullYear()}-${monthAndDay.join('-')}`;
+    },
+
+    getTime(value) {
+      let timeTwoPart = [
+        new Date(value).getUTCHours(),
+        new Date(value).getUTCMinutes(),
+      ]
+        .map((item) => item.toString().padStart(2, '0'))
+        .join(':');
+      let timeThreePart = `${timeTwoPart}:${new Date(value).getUTCSeconds()}`;
+
+      if (new Date(value).getUTCSeconds() % 60 !== 0) {
+        return timeThreePart;
+      } else {
+        return timeTwoPart;
+      }
+    },
+
+    getDateTimeLocal(value) {
+      return `${this.getDate(value)}T${this.getTime(value)}`;
+    },
+  },
+
+  computed: {
+    actualParameter() {
+      if (this.valueAsNumber) {
+        return this.valueAsNumber;
+      } else if (this.valueAsDate) {
+        return this.valueAsDate;
+      } else {
+        return this.value;
+      }
+    },
+
+    formattedDate() {
+      if (this.type === 'time') {
+        return this.getTime(this.actualParameter);
+      } else if (this.type === 'datetime-local') {
+        return this.getDateTimeLocal(this.actualParameter);
+      } else {
+        return this.getDate(this.actualParameter);
+      }
+    },
   },
 };
 </script>
